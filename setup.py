62955a86d2c291cce02edd84600651052a149e35
schuette <schuette.marcus@googlemail.com>
2017-03-02 00:08:27 +0100

    Added api and plugins to provided packages

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ setup(name='server-sleep',
       author='SourceLan',
       author_email='daniel.wiesendorf@googlemail.com, schuette.marcus@googlemail.com',
       license='MIT',
-      packages=['serversleep'],
+      packages=['serversleep',
+                'serversleep.api',
+                'serversleep.coreplugins'],
       scripts=['bin/server-sleep'],
       install_requires=[
-          'server-sleep-api',
-          'server-sleep-coreplugins',
           'configparser'
       ],
       zip_safe=False)
