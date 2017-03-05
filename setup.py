f306b9e7658f7c32e98f542bab8f522fc261d5b4
Daniel Wiesendorf <daniel.wiesendorf@googlemail.com>
2017-03-05 01:05:27 +0100

    coverage reports

--- setup.py
+++ setup.py
@@ -1,18 +1,24 @@
 from setuptools import setup
 
 setup(name='server-sleep',
-      version='0.1',
-      description='server-sleep is a script that suspends your homeserver when it\'s not in use.',
-      url='https://github.com/SLCoding/server-sleep',
-      author='SourceLan',
-      author_email='daniel.wiesendorf@googlemail.com, schuette.marcus@googlemail.com',
-      license='MIT',
-      packages=['serversleep',
-                'serversleep.api',
-                'serversleep.coreplugins'],
-      scripts=['bin/server-sleep'],
-      install_requires=[
-          'configparser',
-          'localconfig'
-      ],
-      zip_safe=False)
+    version='0.1',
+    description='server-sleep is a script that suspends your homeserver when it\'s not in use.',
+    url='https://github.com/SLCoding/server-sleep',
+    author='SourceLan',
+    author_email='daniel.wiesendorf@googlemail.com, schuette.marcus@googlemail.com',
+    license='MIT',
+    packages=[
+        'serversleep',
+        'serversleep.api',
+        'serversleep.coreplugins'],
+    scripts=[
+        'bin/server-sleep'],
+    install_requires=[
+        'configparser',
+        'localconfig'
+    ],
+    setup_requires=[
+        'pytest-runner'],
+    tests_require=[
+        'pytest'],
+    zip_safe=False)
--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,7 +1,9 @@
 # source: http://doc.pytest.org/en/latest/
 
+
 def inc(x):
     return x + 1
+
 
 def test_good():
     assert inc(3) == 4
