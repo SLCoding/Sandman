1577a5eb22fce32ef28ff1192127c1a38ea0327c
Marcus-Matthias Schütte <schuette.marcus@googlemail.com>
2017-03-08 23:33:57 +0100

    Usercheck - shrink unit tests

--- tests/coreplugins/test_user.py
+++ tests/coreplugins/test_user.py
@@ -8,27 +8,27 @@ from unittest.mock import MagicMock
 
 class UserPluginTest(unittest.TestCase):
 
-    def test_create_usercheckplugin(self):
-        configurations = {
+    @staticmethod
+    def get_configuration():
+        return {
             "max_usr": 3,
             "max_usr_local": 2,
             "max_usr_remote": 2,
             "idle_timeout": 3600
         }
-        user_check_plugin = UsercheckPlugin(configurations)
+
+    def test_create_usercheckplugin(self):
+        user_check_plugin = UsercheckPlugin(UserPluginTest.get_configuration())
         self.assertEquals(user_check_plugin.max_usr, 3)
         self.assertEquals(user_check_plugin.max_usr_local, 2)
         self.assertEquals(user_check_plugin.max_usr_remote, 2)
         self.assertEquals(user_check_plugin.idle_timeout, 3600)
 
+    # def test_get_configurables(self):
+    #     user_check_plugin = UsercheckPlugin(self.configurations)
+
     def test_returns_sleep_ready(self):
-        configurations = {
-            "max_usr": 3,
-            "max_usr_local": 2,
-            "max_usr_remote": 2,
-            "idle_timeout": 3600
-        }
-        user_check_plugin = UsercheckPlugin(configurations)
+        user_check_plugin = UsercheckPlugin(UserPluginTest.get_configuration())
         mocked_user_util = UserInformationUtil()
         mocked_user_util.get_local_session_count = MagicMock(return_value=1)
         mocked_user_util.get_remote_session_count = MagicMock(return_value=1)
@@ -38,13 +38,7 @@ class UserPluginTest(unittest.TestCase):
         self.assertEquals(user_check_plugin.check(), CheckReturn.SLEEP_READY)
 
     def test_exceed_max_usr(self):
-        configurations = {
-            "max_usr": 3,
-            "max_usr_local": 3,
-            "max_usr_remote": 3,
-            "idle_timeout": 3600
-        }
-        user_check_plugin = UsercheckPlugin(configurations)
+        user_check_plugin = UsercheckPlugin(UserPluginTest.get_configuration())
         mocked_user_util = UserInformationUtil()
         mocked_user_util.get_local_session_count = MagicMock(return_value=2)
         mocked_user_util.get_remote_session_count = MagicMock(return_value=2)
@@ -54,33 +48,21 @@ class UserPluginTest(unittest.TestCase):
         self.assertEquals(user_check_plugin.check(), CheckReturn.DONT_SLEEP)
 
     def test_exceed_max_local(self):
-        configurations = {
-            "max_usr": 5,
-            "max_usr_local": 1,
-            "max_usr_remote": 4,
-            "idle_timeout": 3600
-        }
-        user_check_plugin = UsercheckPlugin(configurations)
+        user_check_plugin = UsercheckPlugin(UserPluginTest.get_configuration())
         mocked_user_util = UserInformationUtil()
-        mocked_user_util.get_local_session_count = MagicMock(return_value=2)
-        mocked_user_util.get_remote_session_count = MagicMock(return_value=2)
-        mocked_user_util.get_session_count = MagicMock(return_value=4)
+        mocked_user_util.get_local_session_count = MagicMock(return_value=3)
+        mocked_user_util.get_remote_session_count = MagicMock(return_value=0)
+        mocked_user_util.get_session_count = MagicMock(return_value=3)
         user_check_plugin._user_information_util = mocked_user_util
 
         self.assertEquals(user_check_plugin.check(), CheckReturn.DONT_SLEEP)
 
     def test_exceed_max_remote(self):
-        configurations = {
-            "max_usr": 5,
-            "max_usr_local": 4,
-            "max_usr_remote": 1,
-            "idle_timeout": 3600
-        }
-        user_check_plugin = UsercheckPlugin(configurations)
+        user_check_plugin = UsercheckPlugin(UserPluginTest.get_configuration())
         mocked_user_util = UserInformationUtil()
-        mocked_user_util.get_local_session_count = MagicMock(return_value=2)
-        mocked_user_util.get_remote_session_count = MagicMock(return_value=2)
-        mocked_user_util.get_session_count = MagicMock(return_value=4)
+        mocked_user_util.get_local_session_count = MagicMock(return_value=0)
+        mocked_user_util.get_remote_session_count = MagicMock(return_value=3)
+        mocked_user_util.get_session_count = MagicMock(return_value=0)
         user_check_plugin._user_information_util = mocked_user_util
 
         self.assertEquals(user_check_plugin.check(), CheckReturn.DONT_SLEEP)
