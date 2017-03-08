c3b7186e50d96d89e495792734facd49b6bd4ec3
Marcus-Matthias Schütte <schuette.marcus@googlemail.com>
2017-03-08 23:18:31 +0100

    Usercheck - added unit tests

--- serversleep/coreplugins/usercheck.py
+++ serversleep/coreplugins/usercheck.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
 import logging
 import psutil
 from serversleep.api.PluginInterface import AbstractCheckPlugin, CheckReturn, Configurable
@@ -21,23 +20,27 @@ check for users which are logged in
         self.max_usr_remote = self.configuration["max_usr_remote"]
         self.idle_timeout = self.configuration["idle_timeout"]
 
-        self.user_information_util = UserInformationUtil()
+        self._user_information_util = UserInformationUtil()
 
     def __del__(self):
         pass
 
     def check(self):
-        session_count = self.user_information_util.get_session_count(self.idle_timeout)
-        local_session_count = self.user_information_util.get_local_session_count(self.idle_timeout)
-        remote_session_count = self.user_information_util.get_remote_session_count(self.idle_timeout)
+        session_count = self._user_information_util.get_session_count(self.idle_timeout)
+        local_session_count = self._user_information_util.get_local_session_count(self.idle_timeout)
+        remote_session_count = self._user_information_util.get_remote_session_count(self.idle_timeout)
 
         if session_count > self.max_usr:
+            self.logger.info("Too many user sessions. Prevent sleep")
             return CheckReturn.DONT_SLEEP
         if local_session_count > self.max_usr_local:
+            self.logger.info("Too many local user sessions. Prevent sleep")
             return CheckReturn.DONT_SLEEP
         if remote_session_count > self.max_usr_remote:
+            self.logger.info("Too many remote user sessions. Prevent sleep")
             return CheckReturn.DONT_SLEEP
 
+        self.logger.info("Ready for sleep")
         return CheckReturn.SLEEP_READY
 
     def configurables(self):
@@ -47,26 +50,27 @@ check for users which are logged in
                         "Maximum logged in users",
                         "5",
                         "Specify the maximum number of users that should be ignored when putting the Server to sleep."
+                        + " -1 disables check"
                 ),
                 Configurable(
                         "max_usr_local",
                         "Maximum locally logged in users",
                         "2",
                         "Specify the maximum number of local users that should be"
-                        + " ignored when putting the Server to sleep."
+                        + " ignored when putting the Server to sleep. -1 disables check"
                 ),
                 Configurable(
                         "max_usr_remote",
                         "Maximum remote logged in users ",
                         "1",
                         "Specify the maximum number of remote users that should be"
-                        + " ignored when putting the Server to sleep."
+                        + " ignored when putting the Server to sleep. -1 disables check"
                 ),
                 Configurable(
                         "idle_timeout",
                         "User Idle timeout",
                         "3600",
-                        "Specify the idle timeout for user sessions."
+                        "Specify the idle timeout for user sessions. -1 disables timeout"
                 )
         ]
 
--- tests/coreplugins/test_user.py
+++ tests/coreplugins/test_user.py
@@ -1,6 +1,9 @@
 # source: http://doc.pytest.org/en/latest/
 from serversleep.coreplugins.usercheck import UsercheckPlugin
+from serversleep.coreplugins.usercheck import UserInformationUtil
+from serversleep.api.PluginInterface import CheckReturn
 import unittest
+from unittest.mock import MagicMock
 
 
 class UserPluginTest(unittest.TestCase):
@@ -18,3 +21,66 @@ class UserPluginTest(unittest.TestCase):
         self.assertEquals(user_check_plugin.max_usr_remote, 2)
         self.assertEquals(user_check_plugin.idle_timeout, 3600)
 
+    def test_returns_sleep_ready(self):
+        configurations = {
+            "max_usr": 3,
+            "max_usr_local": 2,
+            "max_usr_remote": 2,
+            "idle_timeout": 3600
+        }
+        user_check_plugin = UsercheckPlugin(configurations)
+        mocked_user_util = UserInformationUtil()
+        mocked_user_util.get_local_session_count = MagicMock(return_value=1)
+        mocked_user_util.get_remote_session_count = MagicMock(return_value=1)
+        mocked_user_util.get_session_count = MagicMock(return_value=2)
+        user_check_plugin._user_information_util = mocked_user_util
+
+        self.assertEquals(user_check_plugin.check(), CheckReturn.SLEEP_READY)
+
+    def test_exceed_max_usr(self):
+        configurations = {
+            "max_usr": 3,
+            "max_usr_local": 3,
+            "max_usr_remote": 3,
+            "idle_timeout": 3600
+        }
+        user_check_plugin = UsercheckPlugin(configurations)
+        mocked_user_util = UserInformationUtil()
+        mocked_user_util.get_local_session_count = MagicMock(return_value=2)
+        mocked_user_util.get_remote_session_count = MagicMock(return_value=2)
+        mocked_user_util.get_session_count = MagicMock(return_value=4)
+        user_check_plugin._user_information_util = mocked_user_util
+
+        self.assertEquals(user_check_plugin.check(), CheckReturn.DONT_SLEEP)
+
+    def test_exceed_max_local(self):
+        configurations = {
+            "max_usr": 5,
+            "max_usr_local": 1,
+            "max_usr_remote": 4,
+            "idle_timeout": 3600
+        }
+        user_check_plugin = UsercheckPlugin(configurations)
+        mocked_user_util = UserInformationUtil()
+        mocked_user_util.get_local_session_count = MagicMock(return_value=2)
+        mocked_user_util.get_remote_session_count = MagicMock(return_value=2)
+        mocked_user_util.get_session_count = MagicMock(return_value=4)
+        user_check_plugin._user_information_util = mocked_user_util
+
+        self.assertEquals(user_check_plugin.check(), CheckReturn.DONT_SLEEP)
+
+    def test_exceed_max_remote(self):
+        configurations = {
+            "max_usr": 5,
+            "max_usr_local": 4,
+            "max_usr_remote": 1,
+            "idle_timeout": 3600
+        }
+        user_check_plugin = UsercheckPlugin(configurations)
+        mocked_user_util = UserInformationUtil()
+        mocked_user_util.get_local_session_count = MagicMock(return_value=2)
+        mocked_user_util.get_remote_session_count = MagicMock(return_value=2)
+        mocked_user_util.get_session_count = MagicMock(return_value=4)
+        user_check_plugin._user_information_util = mocked_user_util
+
+        self.assertEquals(user_check_plugin.check(), CheckReturn.DONT_SLEEP)
